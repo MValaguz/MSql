5521d134ab140a37590ef961621e9e1f6ba6d613
Marco Valaguzza <52719603+MValaguz@users.noreply.github.com>
2025-02-05 10:11:43 +0100

    2025_02_05_01
    OK! se faccio CTRL+INVIO e ho del testo selezionato, eseguo direttamente istruzione del testo selezionato; in questo modo uniformo il comportamento di chi normalmente usa questo comando....

--- source/MSql_editor.py
+++ source/MSql_editor.py
@@ -4001,7 +4001,8 @@ class MSql_win2_class(QMainWindow, Ui_MSql_win2):
     def slot_ctrl_invio(self):
         """
            L'utente ha premuto CTRL+Invio: viene ricercata l'istruzione dove il cursore di testo è attualmente posizionato
-           e viene inviata all'esecutore
+           e viene inviata all'esecutore. Se l'utente ha selezionato del testo, viene eseguito il testo selezionato; questo
+           per mantenere un comportamento coerente con l'esecuzione tramite F5.
         """
         # funzione interna creata con CoPilot che dato testo sql e posizione del cursore, restituisce istruzione corrente con posizione di 
         # riga iniziale e finale (caso in cui l'istruzione sia terminata da ;)
@@ -4058,52 +4059,65 @@ class MSql_win2_class(QMainWindow, Ui_MSql_win2):
                     return v_start_pos, v_end_pos, v_riga_inizio, v_riga_fine
             
             return None, None, None, None
+
+        ###
+        # inizio della funzione principale
+        ###
         
-        # prendo posizione cursore in riga e colonna
-        v_pos_line, v_pos_column = self.e_sql.getCursorPosition()
-        # imposto var generali
-        self.v_offset_numero_di_riga = 0
-        # prendo posizione attuale del cursore (relativa!)
-        v_pos_relativa_cursore = self.e_sql.SendScintilla(self.e_sql.SCI_GETCURRENTPOS)
-        # richiamo funzione interna per estrazione dell'istruzione sql (istruzione che termina con ;) e delle relative posizioni di riga
-        v_istruzione, v_start_line, v_end_line = extract_statement_with_positions_comma(self.e_sql.text().upper(), v_pos_relativa_cursore)                
-        # correggo posizioni riga
-        if v_end_line < v_start_line:
-            v_end_line = v_start_line
-        else:
-            v_start_line -= 1
-            v_end_line += 1        
-        
-        # se trovata istruzione che terminava con ;...
-        if v_istruzione != '':
-            # eventuale punto e virgola finale viene tolto
-            if v_istruzione[-1] == ';':            
-                v_istruzione = v_istruzione[0:-1]                                    
-            # eseguo l'istruzione            
+        # controllo se utente ha selezionato del testo specifico che vuole eseguire
+        v_istruzione = self.e_sql.selectedText()        
+        print('!'+v_istruzione+'!')
+        if v_istruzione is not None and v_istruzione != '':
+            print('ciao')
+            # eseguo l'istruzione selezionata                          
             self.esegui_istruzione(v_istruzione, False)        
-            # seleziono il testo per evidenziare l'istruzione che è stata eseguita                                                            
-            if v_start_line < v_end_line:                
-                self.e_sql.setSelection(v_start_line, 0, v_end_line, -1)                           
-        # .. altrimenti
+        # ... altrimenti....
         else:
-            # se non trovato nulla --> provo a fare la ricerca di un'istruzione usando come delimitatori le parole chiave INSERT,UPDATE,DELETE,SELECT
-            # da notare come la stringa venga passata aggiungendo un ritorno a capo in fondo...questo perché se cursore è su ultima istruzione non dava le posizioni corrette
-            v_pos_start, v_pos_end, v_start_line, v_end_line = extract_statement_with_positions_space(self.e_sql.text().upper()+chr(10), v_pos_relativa_cursore)                        
-            if v_pos_start != None and v_pos_end != None:
-                v_istruzione = self.e_sql.text()[v_pos_start:v_pos_end]
-                if v_istruzione != '' and v_istruzione is not None:
-                    # eseguo l'istruzione                           
-                    self.esegui_istruzione(v_istruzione, False)        
-                    # evidenzio il testo dell'istruzione                                                                                                    
+            # prendo posizione cursore in riga e colonna
+            v_pos_line, v_pos_column = self.e_sql.getCursorPosition()
+            # imposto var generali
+            self.v_offset_numero_di_riga = 0
+            # prendo posizione attuale del cursore (relativa!)
+            v_pos_relativa_cursore = self.e_sql.SendScintilla(self.e_sql.SCI_GETCURRENTPOS)
+            # richiamo funzione interna per estrazione dell'istruzione sql (istruzione che termina con ;) e delle relative posizioni di riga
+            v_istruzione, v_start_line, v_end_line = extract_statement_with_positions_comma(self.e_sql.text().upper(), v_pos_relativa_cursore)                
+            # correggo posizioni riga
+            if v_end_line < v_start_line:
+                v_end_line = v_start_line
+            else:
+                v_start_line -= 1
+                v_end_line += 1        
+            
+            # se trovata istruzione che terminava con ;...
+            if v_istruzione != '':
+                # eventuale punto e virgola finale viene tolto
+                if v_istruzione[-1] == ';':            
+                    v_istruzione = v_istruzione[0:-1]                                    
+                # eseguo l'istruzione            
+                self.esegui_istruzione(v_istruzione, False)        
+                # seleziono il testo per evidenziare l'istruzione che è stata eseguita                                                            
+                if v_start_line < v_end_line:                
                     self.e_sql.setSelection(v_start_line, 0, v_end_line, -1)                           
+            # .. altrimenti
+            else:
+                # se non trovato nulla --> provo a fare la ricerca di un'istruzione usando come delimitatori le parole chiave INSERT,UPDATE,DELETE,SELECT
+                # da notare come la stringa venga passata aggiungendo un ritorno a capo in fondo...questo perché se cursore è su ultima istruzione non dava le posizioni corrette
+                v_pos_start, v_pos_end, v_start_line, v_end_line = extract_statement_with_positions_space(self.e_sql.text().upper()+chr(10), v_pos_relativa_cursore)                        
+                if v_pos_start != None and v_pos_end != None:
+                    v_istruzione = self.e_sql.text()[v_pos_start:v_pos_end]
+                    if v_istruzione != '' and v_istruzione is not None:
+                        # eseguo l'istruzione                           
+                        self.esegui_istruzione(v_istruzione, False)        
+                        # evidenzio il testo dell'istruzione                                                                                                    
+                        self.e_sql.setSelection(v_start_line, 0, v_end_line, -1)                           
+                    else:
+                        # altrimenti errore
+                        message_error('No statement found as SELECT, INSERT, UPDATE, DELETE!')
+                        return 'ko'                                    
                 else:
                     # altrimenti errore
                     message_error('No statement found as SELECT, INSERT, UPDATE, DELETE!')
                     return 'ko'                                    
-            else:
-                # altrimenti errore
-                message_error('No statement found as SELECT, INSERT, UPDATE, DELETE!')
-                return 'ko'                                    
 
     def esegui_istruzione(self, p_istruzione, p_explain):
         """
